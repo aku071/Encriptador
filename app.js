6b851ba1b31d0e7074f7d2069f89a17d03005256
Nicolas Foschiatti <ayax071@gmail.com>
2024-08-24 20:03:36 -0300

    Se agrego un poco de style

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-document.querySelector()
+const llavesEncriptador = {
+    e: "enter",
+    i: "imes",
+    a: "ai",
+    o: "ober",
+    u: "ufat"
+}
+
+
+
+const buttons = document.querySelectorAll("button");
+
+butonen.addEventListener("click", createEncriptador); //agrega un evento... pondre el replace aqui
+
+
+
+  //INGRESA DATO. 
+function createEncriptador(llavesEncriptador) {
+  
+    let 
+    return 
+ 
+  }
+  
+
+
+  
